e53820e0e082cc968f083a7d734bbe47916df3a0
Rohan N <nrohan09@gmail.com>
2023-06-19 01:20:09 +0530

    started api integration work

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,40 +1,51 @@
-import useState from 'react'
-import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
-import {useRouter} from 'expo-router'
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
 
-import styles from './popularjobs.style'
-import {COLORS, SIZES} from '../../../constants'
-import PopularjobsCard  from '../../common/cards/popular/PopularJobCard'
+import styles from './popularjobs.style';
+import { COLORS, SIZES } from '../../../constants';
+import PopularjobsCard from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = true;
+  const isLoading = false;
+  const error = false; // Add error state
+
+  const handleRefresh = () => {
+    // Handle refresh logic here, if needed
+  };
+
   return (
-    <View style = {styles.container}>
-      <View style = {styles.header}>
-        <Text style = {styles.headerTitle}>Popular jobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
         <TouchableOpacity>
-          <Text style ={styles.headerBtn}>Show all</Text>
-        </TouchableOpacity>  
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size = "large" colors ={COLORS.primary}/>
-        ) : error ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? ( // Render error message if error state is true
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1,2,3,4,5,6,7,8,9]}
-            renderItem={() => (
+            data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+            renderItem={({ item }) => (
               <PopularjobsCard
-                item = {item}
+                item={item}
               />
             )}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+            refreshing={isLoading} // Set refreshing prop to isLoading to show the ActivityIndicator during refresh
+            onRefresh={handleRefresh} // Handle refresh event
           />
         )}
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;
